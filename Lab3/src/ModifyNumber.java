c3a3a481ef37920b1de87443681ab2cdb7c2d318
Riya Roy <80575275+riyar300@users.noreply.github.com>
2026-01-03 03:30:43 +0530

    Update README.md

--- Lab3/src/ModifyNumber.java
+++ Lab3/src/ModifyNumber.java
@@ -1,9 +1,28 @@
-
+import java.util.Scanner;
+import java.lang.Math;
 public class ModifyNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int number,modifiedNumber;
+		System.out.println("Enter the number:");
+		number=sc.nextInt();
+		modifiedNumber=modifyNumber(number);
+		System.out.println(modifiedNumber);
+	}
 
+	private static int modifyNumber(int number) {
+		String str=String.valueOf(number),str1="";
+		
+		int i,diff;
+		for (i=0;i<str.length()-1;i++) {
+			char c=str.charAt(i);
+			char c1=str.charAt(i+1);
+			diff=Math.abs(Integer.parseInt(String.valueOf(c))-Integer.parseInt(String.valueOf(c1)));
+			str1+=String.valueOf(diff);
+		}
+		str1+=str.charAt(str.length()-1);
+		return Integer.parseInt(str1);
 	}
 
 }
